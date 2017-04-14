ad2f75a3039868410d8c89ae66c7593a5eaa84ed
Sean Macken <royalmackk@hotmail.co.uk>
2017-04-15 00:01:10 +0100

    Adding merge sort

---
+++ Code/Merge Sort.py
@@ -0,0 +1,37 @@
+def mergeSort(alist):   
+    
+    if len(alist) > 1:
+        mid = len(alist) // 2
+        left = alist[:mid]
+        right = alist[mid:]
+
+        mergeSort(left)
+        mergeSort(right)
+
+        i = 0
+        j = 0
+        k = 0
+
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                alist[k] = left[i]
+                i += 1
+            else:
+                alist[k] = right[j]
+                j += 1
+            k += 1
+
+        while i < len(left):
+            alist[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            alist[k] = right[j]
+            j += 1
+            k += 1
+
+alist = [54,26,93,17,77,31,44,55,20]
+print('Before merge: ' , alist)
+mergeSort(alist)
+print('After merge: ' ,alist)
